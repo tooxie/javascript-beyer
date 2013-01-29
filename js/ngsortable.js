e006962abc8774ca2ab55be3836468c1eba326e6
Alvaro Mouriño <alvaro.mourino@deliveryhero.com>
2013-01-29 19:41:52 +0100

    Fixed DnD support for FF

--- js/ngsortable.js
+++ js/ngsortable.js
@@ -2,8 +2,6 @@
 angular.module('directives', [])
 .directive('ngSortable', function() {
     return function(scope, element, attrs, controller) {
-        var dragSourceElement;
-
         console.log('[DIRECTIVE] ngSortable="' + attrs.ngSortable + '"');
 
         scope.$watch(attrs.ngSortable, function(values) {
@@ -13,12 +11,14 @@ angular.module('directives', [])
             while (x--) {
                 var track = tracks[x]
                     dropzone = document.getElementById('dropzone');
+
                 if (track.draggable && !track.handled) {
                     track.handled = true;
                     // dragstart
                     track.addEventListener('dragstart', function (e) {
                         dropzone.ignoreDrag = true;
-                        dragSourceElement = this;
+                        e.dataTransfer.setData('trackid', e.target.id);
+                        e.dataTransfer.setData('text/html', null);
                         this.style.opacity = '0.4';
                     }, false)
 
@@ -55,8 +55,8 @@ angular.module('directives', [])
                     track.addEventListener('drop', function (e) {
                         var source = -1,
                             dest = -1,
-                            tracks = Beyer.Playlist.trackList,
-                            srcName = dragSourceElement.children[0].innerText,
+                            tracks = document.getElementById('tracklist').children,
+                            el = document.getElementById(e.dataTransfer.getData('trackid')),
                             x = tracks.length;
 
                         this.classList.remove('over');
@@ -66,7 +66,7 @@ angular.module('directives', [])
                             e.stopPropagation();
                         }
 
-                        if (dragSourceElement == this) {
+                        if (el.id == e.target.id) {
                             return false;
                         }
 
@@ -74,15 +74,15 @@ angular.module('directives', [])
                             // The dragSourceElement is a hack because the DnD
                             // API doesn't provide the element being dropped,
                             // so we have to store it in some freakkin global.
-                            if (tracks[x].name == srcName) {
+                            if (tracks[x].id == el.id) {
                                 source = x;
                             }
-                            if (tracks[x].name == e.target.innerText) {
+                            if (tracks[x].id == e.target.id) {
                                 dest = x;
                             }
                         }
 
-                        console.log('Moving track "' + srcName + '" from ' + source + ' to ' + dest);
+                        console.log('Moving track #"' + el.id + '" from ' + source + ' to ' + dest);
 
                         if ((source != dest) && (source != -1 && dest != -1)) {
                             Beyer.Playlist.move(source, dest);
